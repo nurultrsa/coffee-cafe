63250c58add0bd70800b90d85f8c592457b634da
Nurul Trisna Aulya <aulyaatn@gmail.com>
2021-10-23 14:26:09 +0700

    Remove script for review (cancelled to be included)

--- assets/js/script.js
+++ assets/js/script.js
@@ -42,21 +42,6 @@ function loginFunction() {
   }
 }
 
-/*Script for review*/
-const btn = document.querySelector("button-post");
-const post = document.querySelector(".post");
-const widget = document.querySelector(".star-widget");
-const editBtn = document.querySelector(".edit");
-    btn.onclick = () => {
-    widget.style.display = "none";
-    post.style.display = "block";
-    editBtn.onclick = () => {
-    widget.style.display = "block";
-    post.style.display = "none";
-    }
-      return false;
-    }
-
 /*Script for working hours cafe*/
 function opening_hours() {
   let date = new Date();
